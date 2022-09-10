d0f96345994a2db42dfa10c4d52ca786f997d281
mercy bassey <udohmercy911@gmail.com>
2022-09-10 13:06:29 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+
+const express = require('express')
+
+const app = express()
+
+const port  = process.env.PORT || 4000;
+
+app.get('/', (req, res) => {
+  res.status(200).json('Hello world')
+})
+
+
+module.exports = app.listen(port, () => console.log(`Running on ${port}`))
